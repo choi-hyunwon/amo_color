dc2270bff90b7c4ab8a5731339220d70b17670d6
Yujung Jung <darron@nate.com>
2020-09-02 14:52:04 +0900

    페이지 리스트 추가

---
+++ pagelist/data/data.js
@@ -0,0 +1,25 @@
+var data = {}
+data.common = {
+	'title': 'Amore Color Tailor',
+	'list': [
+		{
+			'stateClass': 'complete',
+			'url': 'my',
+			'category': 'My',
+			'page': '마이페이지'
+		},
+		{
+			'stateClass': 'complete',
+			'url': 'brandList',
+			'category': 'Brand',
+			'page': '브랜드'
+		},
+		{
+			'stateClass': 'complete',
+			'url': 'talk',
+			'category': 'Talk',
+			'page': '토크'
+		}
+	]
+}
+
